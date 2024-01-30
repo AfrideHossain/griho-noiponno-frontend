2b987d86f5d89e0cc9ad22a3db3d9275405c2c2a
AfrideHossain <asafridehossain171@gmail.com>
2024-01-30 22:07:30 +0600

    Shop side bar and product card added

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,10 +1,14 @@
 import SearchBox from "./SearchBox/SearchBox";
+import SideBar from "./SideBar/SideBar";
 
 const Shop = () => {
   return (
-    <div className="md:max-w-7xl p-4 mx-auto">
+    <div className="md:max-w-7xl h-full p-4 mx-auto">
       <SearchBox />
-      Shop
+      <div className="w-full h-full flex gap-10 justify-start items-start mt-20">
+        <SideBar />
+        Shop
+      </div>
     </div>
   );
 };
---
+++ src/Pages/Shop/SideBar/SideBar.jsx
@@ -0,0 +1,25 @@
+const SideBar = () => {
+  return (
+    <div className="w-96 bg-white bg-opacity-10 backdrop-blur-md p-4 rounded-md">
+      <div className="w-full">
+        <h2 className="text-xl font-semibold mb-4">Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+        <h2>Categories</h2>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
