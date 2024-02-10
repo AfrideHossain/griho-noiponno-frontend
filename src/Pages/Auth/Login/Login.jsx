14775769111dad5641534bd1f1d3968bea2dae74
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2024-02-10 21:52:42 +0600

    login page updated

--- src/Pages/Auth/Login/Login.jsx
+++ src/Pages/Auth/Login/Login.jsx
@@ -7,21 +7,32 @@ const Login = () => {
         alt="Background image for login page"
         className="w-full h-full object-cover -z-10"
       />
-      <div className="w-full h-full absolute top-0 left-0 bg-black bg-opacity-50 z-0 flex justify-center items-center">
-        <div className="border rounded-md p-4 bg-white bg-opacity-10 backdrop-blur-2xl">
-          <div className="form w-full space-y-3">
-            <div className="input-section">
-              <input type="text" placeholder="Username (E.g: johndoe)" />
-            </div>
-            <div className="input-section">
-              <input
-                type="email"
-                placeholder="email (E.g: johndoe@email.com)"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary w-full">
-              Login
-            </button>
+      <div className="w-full h-full absolute top-0 left-0 bg-black bg-opacity-50 z-0 flex justify-center items-center backdrop-blur-md">
+        <div className="bg-gray-800 backdrop-blur-2xl max-w-4xl flex items-center justify-between gap-5">
+          {/* Image section */}
+          <div>
+            <img
+              src="https://i.ibb.co/dkwWBSC/kam-idris-Hq-HX3-LBN18-unsplash.jpg"
+              alt="log in page image"
+            />
+          </div>
+
+          {/* form section */}
+          <div>
+            <form className="form w-full space-y-3">
+              <div className="input-section">
+                <input type="text" placeholder="Username (E.g: johndoe)" />
+              </div>
+              <div className="input-section">
+                <input
+                  type="email"
+                  placeholder="email (E.g: johndoe@email.com)"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary w-full">
+                Login
+              </button>
+            </form>
           </div>
         </div>
       </div>
