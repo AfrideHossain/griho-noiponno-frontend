bbf624d6bda304944e5f3e20dcaf25a5021fa8e0
AfrideHossain <asafridehossain171@gmail.com>
2024-02-13 16:43:32 +0600

    login page updated

--- src/Pages/Auth/Login/Login.jsx
+++ src/Pages/Auth/Login/Login.jsx
@@ -10,18 +10,18 @@ const Login = () => {
       />
       <div className="w-full h-full absolute top-0 left-0 bg-black bg-opacity-50 z-0 flex justify-center items-center backdrop-blur-md">
         {/* form container */}
-        <div className="max-w-5xl md:w-[1024px] relative flex items-start justify-between gap-10">
+        <div className="max-w-5xl md:w-[1024px] relative flex items-start justify-between gap-10 rounded-2xl border border-gray-600 shadow-xl">
           {/* Image section */}
           <div className="flex-grow form-side-image-section absolute top-0 left-0 -z-10">
             <img
               src="https://i.ibb.co/dkwWBSC/kam-idris-Hq-HX3-LBN18-unsplash.jpg"
               alt="log in page image"
-              className="form-image"
+              className="form-image rounded-xl"
             />
           </div>
 
           {/* form section */}
-          <div className="p-10 h-full bg-gray-800 backdrop-blur-2xl ms-auto">
+          <div className="p-10 h-full bg-gray-800 backdrop-blur-2xl ms-auto rounded-r-xl">
             {/* texts section */}
             <div className="w-full">
               <h3 className="text-2xl font-bold mb-20">Griho Noiponno</h3>
